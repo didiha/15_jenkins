d03a0002554fec085a9a793c318c129a68c77379
didiha <hcring@naver.com>
2025-05-25 23:31:02 +0900

    test2

--- src/main/java/com/hcring/jenkins/controller/CalculatorController.java
+++ src/main/java/com/hcring/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "jenkins pipeline test1";
+        return "jenkins pipeline test2";
     }
 
     @GetMapping("/plus")
