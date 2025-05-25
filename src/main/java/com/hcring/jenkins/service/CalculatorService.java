b29104afbb8e08c3eafd9f974ccbe31efd46842c
didiha <hcring@naver.com>
2025-05-25 18:15:42 +0900

    first commit

---
+++ src/main/java/com/hcring/jenkins/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.hcring.jenkins.service;
+
+import com.hcring.jenkins.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
